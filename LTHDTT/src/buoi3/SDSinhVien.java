df5857c7dec002b74703424fa12d2b39142cdee4
Zng Tfy <nvt87x@gmail.com>
2018-11-07 20:38:39 +0700

    Sinh vien

--- LTHDTT/src/buoi3/SDSinhVien.java
+++ LTHDTT/src/buoi3/SDSinhVien.java
@@ -5,33 +5,35 @@ import java.util.Scanner;
 public class SDSinhVien {
 
 	public static void main(String[] args) {
-		SinhVien a = new SinhVien();
-		a.nhap();
-		a.themHocPhan(mon, d);
-		a.in();
-		
-		
+		/*SinhVien a = new SinhVien();
+		a.nhap();		
+		a.themHocPhan("Toan", "A");
+		a.in();*/
+
 		SinhVien ds[];
 		Scanner sc = new Scanner(System.in);
 		int n;
-		System.out.print("Nhap so sinh vien:");
+		System.out.print("Nhap so luong sinh vien: ");
 		n = sc.nextInt();
 		ds = new SinhVien[n];
-		System.out.print("Nhap danh sach sinh vien:");
-		sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			System.out.print("Nhap sinh vien thu" + (i + 1) + ":");
-			ds[i].nhap();
 		
-        for(SinhVien s: ds) {
-        	System.out.print("Thong tin sinh vien thu" + (i + 1) + ":"+ds[i]);
-			ds[i].in();
-        }}
+		System.out.print("Nhap danh sach sinh vien: ");
+		sc.nextLine();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print("Nhap sinh vien thu " + (i + 1) + ": ");
+			ds[i].nhap();
+
+			for (SinhVien s : ds) {
+				System.out.print("Thong tin sinh vien thu " + (i + 1) + ": " + ds[i]);
+				ds[i].in();
+			}
+		}
+		
 		System.out.print("Nhap diem cho cac sinh vien:");
 		for (int i = 0; i < ds.length; i++) {
-			System.out.print("Nhap sinh vien thu" + (i + 1) + ":");
+			System.out.print("Nhap sinh vien thu " + (i + 1) + ": ");
 			ds[i].nhapDiem();
-			}
-        
+		}
 	}
 }
--- LTHDTT/src/buoi3/SinhVien.java
+++ LTHDTT/src/buoi3/SinhVien.java
@@ -6,129 +6,122 @@ import buoi2.Date;
 
 public class SinhVien {
 	private String mssv;
-	private String hoten;
-	private Date ngaysinh;
-	private int slhphan;
-	private String hphan[];
+	private String hoTen;
+	private Date ngaySinh;
+	private int slHocPhan;
+	private String hocPhan[];
 	private String diem[];
 	private int max = 55;
 
 	public SinhVien() {
 		mssv = new String();
-		hoten = new String();
-		ngaysinh = new Date();
-		slhphan = 0;
-		hphan = new String[max];
+		hoTen = new String();
+		ngaySinh = new Date();
+		slHocPhan = 0;
+		hocPhan = new String[max];
 		diem = new String[max];
-
 	}
 
-	public SinhVien(String mssv1, String hoten1, Date ngaysinh1, int max1) {
-		mssv = new String(mssv1);
-		hoten = new String(hoten1);
-		ngaysinh = new Date(ngaysinh1);
-		max = max1;
+	public SinhVien(String mssv, String hoTen, Date ngaySinh, int max) {
+		this.mssv = new String(mssv);
+		this.hoTen = new String(hoTen);
+		this.ngaySinh = new Date(ngaySinh);
+		this.max = max;
 	}
 
 	public SinhVien(SinhVien obj) {
-		mssv = new String(obj.mssv);
-		hoten = new String(obj.hoten);
-		ngaysinh = new Date(obj);
-		slhphan = obj.slhphan;
-		hphan = new String[obj.max];
-		diem = new String[obj.max];
-
+		this.mssv = new String(obj.mssv);
+		this.hoTen = new String(obj.hoTen);
+		this.ngaySinh = new Date(obj.ngaySinh);
+		this.slHocPhan = obj.slHocPhan;
+		this.hocPhan = new String[obj.max];
+		this.diem = new String[obj.max];
 	}
 
 	public void nhap() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Nhap ma so sinh vien:");
+		System.out.print("Nhap ma so sinh vien: ");
 		mssv = sc.nextLine();
-		System.out.print("Nhap ho ten:");
-		hoten = sc.nextLine();
-		System.out.print("Nhap ngay sinh:");
-		ngaysinh.nhap();
+		System.out.print("Nhap ho ten: ");
+		hoTen = sc.nextLine();
+		System.out.print("Nhap ngay sinh: ");
+		ngaySinh.nhap();
 	}
 
 	public void nhapDiem() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("so mon can nhap");
-		slhphan = sc.nextInt();
-		for (int i = 0; i < slhphan; i++) {
-			hphan[i] = new String();
+		System.out.print("So mon can nhap: ");
+		slHocPhan = sc.nextInt();
+
+		for (int i = 0; i < slHocPhan; i++) {
+			hocPhan[i] = new String();
 			diem[i] = new String();
-			System.out.print("Nhap ten mon thu" + (i + 1) + ":");
-			hphan[i] = sc.nextLine();
-			System.out.print("Nhap diem mon hoc:");
+			System.out.print("Nhap ten mon thu " + (i + 1) + ": ");
+			hocPhan[i] = sc.nextLine();
+			System.out.print("Nhap diem mon hoc: ");
 			diem[i] = sc.nextLine();
 		}
-
 	}
 
 	public void in() {
-		System.out.print("mssv:" + mssv);
-		System.out.print("hoten:" + hoten);
-		System.out.print("ngaysinh:" + ngaysinh);
-		for (int i = 0; i < slhphan; i++) {
-			System.out.print(hphan[i] + ":" + diem[i]);
-
-		}
+		System.out.print(toString());
 	}
 
 	public String toString() {
 		String s = " ";
-		s = ("mssv:" + mssv + "ho ten:" + hoten + "ngaysinh:" + ngaysinh);
-		for (int i = 0; i < slhphan; i++) {
-			s = s + hphan[i] + diem[i];
+		s = "MSSV: " + mssv + " Ho ten: " + hoTen + " Ngay sinh: " + ngaySinh.toString() + " -- ";
 
+		for (int i = 0; i < slHocPhan; i++) {
+			s = s + hocPhan[i] + diem[i];
 		}
-		return s;
 
+		return s;
 	}
 
 	public void themHocPhan(String mon, String d) {
-		if(slhphan<max-1) {// em khong hieu
-			for (int i = 0; i < slhphan; i++) {
-			hphan[i]=new String(mon);
-			diem[i]=new String(d);
+		if (slHocPhan <= max) {// em khong hieu
+			for (int i = 0; i < slHocPhan; i++) {
+				hocPhan[i] = new String(mon);
+				diem[i] = new String(d);
+			}
+		} else {
+			System.out.print("Loi! Khong the dang ki them hoc phan");
+		}
+	}
+
+	public void xoaHocPhan(String mon) {
+		for (int i = 0; i < slHocPhan; i++) {
+			if (mon.compareTo(hocPhan[i]) == 0) {// 2 cai nay deu kieu String ma sao no bao loi la k ss duoc
+				break;
+			}
+
+			if (i < slHocPhan) {
+				for (int j = i; j < slHocPhan - 1; j++) {
+					hocPhan[i] = hocPhan[j - 1];
+					diem[i] = diem[j - 1];
+				}
+				slHocPhan--;
 			}
 		}
-		else System.out.print(" loi! khong the dang ki them hoc phan");
 	}
-	public void xoaHocPhan(String mon) {
-		for (int i = 0; i < slhphan; i++) {
-		    if(mon.compareTo(hphan[i]==0) {// 2 cai nay deu kieu String ma sao no bao loi la k ss duoc
-			   break;
-		       }
-		    if(i<slhphan)	{
-			   for (int j=i; j<slhphan-1; j++) {
-				hphan[i]= hphan[j-1];
-				diem[i]= diem[j-1];
-			   }
-			   slhphan--;
-		    }  
-		}
-	}
+
 	public float tinhDiemTB() {
-		float dtb=0.0f;
-		for (int i = 0; i < slhphan; i++) {
-		    if(diem[i].compareTo("A")) {
-		    	dtb=dtb+4;
-		    }
-		    else if (diem[i].compareTo("B")) {
-		    	dtb=dtb+3;
-		    }
-		    else if (diem[i].compareTo("B+")) {
-		    	dtb=dtb+3.5;
-		    }
-		    else if (diem[i].compareTo("C")) {
-		    	dtb=dtb+2.5;
-		    }
-		    else if((diem[i].compareTo("D")) {
-		    	dtb=dtb+2;
-		    }
-		    return dtb;
-		
+		float dtb = 0.0f;
+
+		for (int i = 0; i < slHocPhan; i++) {
+			if (diem[i].compareTo("A") == 0) {
+				dtb = dtb + 4;
+			} else if (diem[i].compareTo("B") == 0) {
+				dtb = dtb + 3;
+			} else if (diem[i].compareTo("B+") == 0) {
+				dtb = dtb + 3.5f;
+			} else if (diem[i].compareTo("C") == 0) {
+				dtb = dtb + 2.5f;
+			} else if (diem[i].compareTo("D") == 0) {
+				dtb = dtb + 2;
+			}
+		}
+
+		return dtb;
 	}
-}
 }
